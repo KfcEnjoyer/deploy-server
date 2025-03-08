cb375c344e46e3d64b202c3183459939453831a5
KfcEnjoyer <alikhan_azanbaev@mail.ru>
2025-03-07 23:11:07 -0500

    Initial commit - client code only

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const express = require("express");
 const app = express();
-
-// CORS middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://al-bank.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+const cors = require('cors');
 
 app.use(express.json());
 
-// Test route
-app.get('/test', (req, res) => {
-  res.json({ message: 'CORS test successful' });
-});
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
+const corsOptions = {
+  origin: "https://al-bank.netlify.app", // You can use a string if there's only one origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Authorization", "Content-Type"],
+  credentials: true // Add this if you're using cookies or authentication
+};
 
-app.get('/', (req, res) => {
-  res.status(200).send('Bank API is running!');
-});
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(cors(corsOptions));
+
+const db = require("./models");
+
+// Routers
+const profile = require("./routes/Profile");
+app.use('/profile', profile);
+const authRoute = require('./routes/Authenticate');
+app.use('/auth', authRoute);
+const openAccount = require("./routes/openAccount");
+app.use("/openaccount", openAccount);
+const transfer = require("./routes/Transfer");
+app.use("/transfer", transfer);
+const details = require("./routes/AccountDetails");
+app.use("/accounts", details);
+const transaction = require("./routes/Transaction");
+app.use("/transactions", transaction);
+const stocks = require('./routes/Stocks');
+app.use('/api/stocks', stocks);
+
+
+
+db.sequelize.sync().then(() => {
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
